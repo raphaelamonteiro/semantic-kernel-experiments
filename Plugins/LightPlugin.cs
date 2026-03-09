5a412a74123039241e98597793432ae10ce2b08c
Raphaela Monteiro <raphaela.monteiro@waveconcept.com.br>
2026-03-09 17:55:31 -0300

    feat: add llama3

---
+++ Plugins/LightPlugin.cs
@@ -0,0 +1,15 @@
+namespace Plugins
+{
+    public class LightPlugin
+    {
+        public string TurnOn()
+        {
+            return "💡 Light is now ON";
+        }
+
+        public string TurnOff()
+        {
+            return "🔌 Light is now OFF";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.SemanticKernel;
+
+var builder = Kernel.CreateBuilder();
+
+builder.AddOllamaChatCompletion(
+    modelId: "llama3",
+    endpoint: new Uri("http://localhost:11434")
+);
+
+
+
+var kernel = builder.Build();
+
+var result = await kernel.InvokePromptAsync("Explain Semantic Kernel in one sentence");
+
+
+
+Console.WriteLine(result);
