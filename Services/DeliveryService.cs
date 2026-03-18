05c11484439808e35185e5f764bf58bd0d3dc03e
Raphaela Monteiro <raphaela.monteiro@waveconcept.com.br>
2026-03-18 15:29:18 -0300

    fix: update deliveryService with token and request

--- Services/DeliveryService.cs
+++ Services/DeliveryService.cs
@@ -1,4 +1,5 @@
 using RestSharp;
+using System.Text.Json;
 
 public class DeliveryApiService
 {
@@ -7,14 +8,51 @@ public class DeliveryApiService
     public DeliveryApiService()
     {
         _client = new RestClient("http://localhost:5256");
-    }
 
-    public async Task<string> GetProdutosAsync()
+        var token = Environment.GetEnvironmentVariable("API_TOKEN");
+
+        if (string.IsNullOrEmpty(token))
+            throw new Exception("API_TOKEN não configurado");
+
+        _client.AddDefaultHeader("Authorization", $"Bearer {token}");
+    }
+    public async Task<List<ProdutoDto>?> BuscarProdutosAsync(
+        string? nome = null,
+        int operador = 1)
     {
-        var request = new RestRequest("/api/produtos", Method.Get);
+        var request = new RestRequest("/Produto/Consultar", Method.Post);
+
+        var body = new ConsultaDto
+        {
+            RegistrosPorPagina = 10,
+            NumeroPagina = 1,
+            ListPesquisaDto = string.IsNullOrEmpty(nome)
+                ? new List<PesquisaDto>()
+                : new List<PesquisaDto>
+                {
+                new PesquisaDto
+                {
+                    AtributoPesquisa = "descricao",
+                    Operador = operador,
+                    ValorPesquisa = nome
+                }
+                }
+        };
+
+        request.AddJsonBody(body);
 
         var response = await _client.ExecuteAsync(request);
 
-        return response.Content ?? "";
+        if (!response.IsSuccessful || string.IsNullOrEmpty(response.Content))
+        {
+            Console.WriteLine($"Erro API: {response.StatusCode}");
+            Console.WriteLine(response.Content);
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<List<ProdutoDto>>(
+            response.Content,
+            new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+        );
     }
 }
